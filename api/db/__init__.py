a1650c6e78e5f1837a57788a1a708f318eb08767
Lokendra Bohra <2017kucp1038@iiitkota.ac.in>
2021-04-22 13:24:51 +0530

    + Dot-env

--- api/db/__init__.py
+++ api/db/__init__.py
@@ -1,2 +1,10 @@
 from fastapi_quickstart import init_db
-engine, Base, get_db = init_db('localhost', 'postgres', '1704', 'dummy')
+import os
+
+dbhost = os.environ["POSTGRE_HOSTNAME"]
+dbport = os.environ["POSTGRE_PORT_NO"]
+dbuser = os.environ["POSTGRE_USERNAME"]
+dbpass = os.environ["POSTGRE_PWD"]
+dbname = os.environ["POSTGRE_DBNAME"]
+
+engine, Base, get_db = init_db(dbhost, dbuser, dbpass, dbname)
