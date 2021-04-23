6af03a150569841beb882b34578902d898e904b1
Lokendra Bohra <2017kucp1038@iiitkota.ac.in>
2021-04-23 16:24:35 +0530

    ++

--- api/main.py
+++ api/main.py
@@ -1,8 +1,13 @@
+from dotenv import load_dotenv
 from .models.user import User
 from .db import Base, engine
 from fastapi import FastAPI
 from .endpoints import user, login
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    load_dotenv(verbose=True)
 
 # Include all the Models here
 Base.metadata.create_all(bind=engine)
